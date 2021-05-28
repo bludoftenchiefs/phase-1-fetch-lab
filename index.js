fbc100629d4fa729020e827c9076fa088a9a9b87
rilith <bludoftenchiefs@protonmail.com>
2021-05-27 21:34:11 -0500

    commit tree c69b0b873b0386beb1ee99a3ddf83aaaceeaee94

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+fetch('https://anapioficeandfire.com/api/books') 
+  .then(function(response){
+    return response.json();
+  }) .then(function(json){
+    console.log(json);
+    renderBooks(json);
+  })
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -13,5 +20,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks()
 });
+
